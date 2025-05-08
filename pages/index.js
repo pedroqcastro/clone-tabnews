9e58752d7846950e3f08e643e3cfc3db0b19b560
Pedro Lucas <pedrocastrinholucas@gmail.com>
2025-05-08 14:06:04 +0000

    Alteração da mensagem principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-  return <h1>Teste ?</h1>
+  return <h1>Oi, Cassie! Estou aprendendo programação, assim como você 😊!
+  <br/>A jornada é longa e difícil, mas temos um ao outro para nos ajudar!
+  <br/>Conta comigo gatinha 😘
+  </h1>
 }
 
 export default Home;
