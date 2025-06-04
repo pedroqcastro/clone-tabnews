aa34935cacf3a593f413b4957ed8fe89ef4c21d2
Pedro Lucas <pedrocastrinholucas@gmail.com>
2025-06-04 01:41:55 +0000

    Emoji adicionado a mensagem principal no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return <h1>Oi, Cassie! Estou aprendendo programação, assim como você 😊!
   <br/>A jornada é longa e difícil, mas temos um ao outro para nos ajudar!
-  <br/>Conta comigo gatinha 😘
+  <br/>Conta comigo gatinha 😘 💖
   </h1>
 }
 
