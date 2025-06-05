6184f7dabb8ebd3b6a6f280700d2d0ab2d16d45d
Pedro Lucas <pedrocastrinholucas@gmail.com>
2025-06-05 16:57:17 +0000

    Adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 function Home() {
-  return <h1>Oi, Cassie! Estou aprendendo programação, assim como você 😊!
-  <br/>A jornada é longa e difícil, mas temos um ao outro para nos ajudar!
-  <br/>Conta comigo gatinha 😘 💖
-  </h1>
+  return (
+    <h1>
+      Oi, Cassie! Estou aprendendo programação, assim como você 😊!
+      <br />A jornada é longa e difícil, mas temos um ao outro para nos ajudar!
+      <br />
+      Conta comigo gatinha 😘 💖
+    </h1>
+  );
 }
 
 export default Home;
