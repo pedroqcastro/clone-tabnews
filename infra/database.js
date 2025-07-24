e5b8e75101acace163d73ec6ddfb2af8e4bc817f
Pedro Lucas <pedrocastrinholucas@gmail.com>
2025-07-24 12:48:53 +0000

    make `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,16 +9,26 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log('Credenciais do banco:',{
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  })
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log(`Houve um erro ao tentar realizar a consulta no banco: ${error}`);
+    console.error(`Houve um erro ao tentar realizar a consulta no banco: ${error}`);
+    throw error;
   } finally {
     await client.end();
   };
 }
+
 
 export default {
   query: query,
