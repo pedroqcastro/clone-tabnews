c99e563ab3ebdfa7ae46e6c81f135875dc942e86
Pedro Lucas <pedrocastrinholucas@gmail.com>
2025-07-29 14:24:37 +0000

    getting rid of database credencials log

--- infra/database.js
+++ infra/database.js
@@ -10,16 +10,6 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  console.log('Credenciais do banco:',{
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-    node_env: process.env.NODE_ENV,
-  })
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
