d0aedce654c09b7fcebeff2276a458845fbb496b
Pedro Lucas <pedrocastrinholucas@gmail.com>
2025-10-15 12:14:06 +0000

    adds opened  connections log  in the  migrations  index

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -35,5 +35,12 @@ export default async function migrations(request, response) {
         return response.status(200).json(migratedMigrations);
     }
 
+    const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]});
+    
+    const databaseOpenedConnectionsValue = parseInt(databaseOpenedConnectionsResult.rows[0].count);
+
+    console.log("conexoes abertas: " + databaseOpenedConnectionsValue)
     return response.status(405).end();
 }
